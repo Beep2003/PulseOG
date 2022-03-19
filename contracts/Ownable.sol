42d3c7b0e1e6943ba003cb904568591e62ef31fa
/dhsu47389HJHD*9* <aeCuulmXyStuH1S390ntWjSfk@protonmail.com>
2022-03-19 15:35:49 -0400

    Added contracts folder
    Added contracts folder with three .sol files for fuctionality of PulseOG token

---
+++ contracts/Ownable.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+
+contract Ownable {
+    
+    address private _owner;
+
+   
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+   
+    modifier onlyOwner() {
+        require(_owner == msg.sender, "Ownable: only owner can call this function");
+       
+        _;
+    }
+
+    constructor() {
+        _owner = msg.sender;
+        emit OwnershipTransferred(address(0), _owner);
+    }
+  
+    function owner() public view returns(address) {
+        return _owner;
+
+    }
+
+    
+    function renounceOwnership() public onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+   
+    function transferOwnership(address newOwner) public onlyOwner {
+        _transferOwnership(newOwner);
+    }
+    
+    function _transferOwnership(address newOwner) internal {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+
+}
---
+++ contracts/Stakeable.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+
+contract Stakeable {
+
+
+    
+    constructor() {
+        
+        stakeholders.push();
+    }
+    
+    struct Stake{
+        address user;
+        uint256 amount;
+        uint256 since;
+        uint256 claimable;
+    }
+    
+    struct Stakeholder{
+        address user;
+        Stake[] address_stakes;
+        
+    }
+
+    
+     struct StakingSummary{
+         uint256 total_amount;
+         Stake[] stakes;
+     }
+
+
+    
+    Stakeholder[] internal stakeholders;
+    
+    mapping(address => uint256) internal stakes;
+   
+     event Staked(address indexed user, uint256 amount, uint256 index, uint256 timestamp);
+
+   
+    uint256 internal rewardPerHour = 1000;
+
+    
+    function _addStakeholder(address staker) internal returns (uint256){
+        
+        stakeholders.push();
+        
+        uint256 userIndex = stakeholders.length - 1;
+        
+        stakeholders[userIndex].user = staker;
+        
+        stakes[staker] = userIndex;
+        return userIndex; 
+    }
+
+   
+    function _stake(uint256 _amount) internal{
+         
+        require(_amount > 0, "Cannot stake nada");
+        
+
+        
+        uint256 index = stakes[msg.sender];
+        
+        uint256 timestamp = block.timestamp;
+        
+        if(index == 0){
+           
+            index = _addStakeholder(msg.sender);
+        }
+
+        
+        stakeholders[index].address_stakes.push(Stake(msg.sender, _amount, timestamp,0));
+        
+        emit Staked(msg.sender, _amount, index,timestamp); 
+    }
+
+    
+      function calculateStakeReward(Stake memory _current_stake) internal view returns(uint256){
+         
+          return (((block.timestamp - _current_stake.since) / 1 hours) * _current_stake.amount) / rewardPerHour;
+      }
+
+    
+     function _withdrawStake(uint256 amount, uint256 index) internal returns(uint256){
+         
+        uint256 user_index = stakes[msg.sender];
+        Stake memory current_stake = stakeholders[user_index].address_stakes[index];
+        require(current_stake.amount >= amount, "Staking: Cannot withdraw more than you have staked silly billy");
+
+         
+         uint256 reward = calculateStakeReward(current_stake);
+         
+         current_stake.amount = current_stake.amount - amount;
+         
+         if(current_stake.amount == 0){
+             delete stakeholders[user_index].address_stakes[index];
+         }else {
+             
+             stakeholders[user_index].address_stakes[index].amount = current_stake.amount;
+            
+            stakeholders[user_index].address_stakes[index].since = block.timestamp;    
+         }
+
+         return amount+reward;
+
+     }
+
+     
+    function hasStake(address _staker) public view returns(StakingSummary memory){
+       
+        uint256 totalStakeAmount; 
+       
+        StakingSummary memory summary = StakingSummary(0, stakeholders[stakes[_staker]].address_stakes);
+        
+        for (uint256 s = 0; s < summary.stakes.length; s += 1){
+           uint256 availableReward = calculateStakeReward(summary.stakes[s]);
+           summary.stakes[s].claimable = availableReward;
+           totalStakeAmount = totalStakeAmount+summary.stakes[s].amount;
+       }
+       
+       summary.total_amount = totalStakeAmount;
+        return summary;
+    }
+
+
+
+
+}
