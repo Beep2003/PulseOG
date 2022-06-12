a3b3d7dff8b57cf5d03e81e22fdb5f60508de73d
/dhsu47389HJHD*9* <101935907+Beep2003@users.noreply.github.com>
2022-06-12 12:20:39 -0400

    PulseOG total supply decrease

--- migration/1_deploy_pulseog.js
+++ migration/1_deploy_pulseog.js
@@ -2,7 +2,7 @@ const PulseOG = artifacts.require("PulseOG");
 
 module.exports = async function (deployer, network, accounts) {
   
-  await deployer.deploy(PulseOG, "PulseOG", "POG", 18, "21000000000000000000000000000000");
+  await deployer.deploy(PulseOG, "PulseOG", "POG", 18, "21000000000000000000000000000");
   const pulseOG = await PulseOG.deployed()
 
 };
