f316c98281dbf61715088f2e507d94257f95808c
/dhsu47389HJHD*9* <101935907+Beep2003@users.noreply.github.com>
2022-08-03 11:38:22 -0400

    Add PulseOG Origin Address :)

---
+++ zcontract_creator/pulseog_origin_address.js
@@ -0,0 +1,9 @@
+{
+    
+    
+    `0xBd98B16b7bAf7fe2eB6fA57D6BFc77bC4b7590DC`
+
+    "Official PulseOG Origin Address."
+    "Any contract creation or transaction from above address you can trust :)."
+    "PulseOG has many test wallets spanning across many blockchains, trust none of those addresses for PulseChain mainnet."
+}
