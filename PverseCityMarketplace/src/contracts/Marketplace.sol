2a9e5127387a4a616d36f44d0c96dc73e31e2435
/dhsu47389HJHD*9* <101935907+Beep2003@users.noreply.github.com>
2023-03-14 20:43:20 -0400

    Upgrade Pverse City Marketplace Contract

--- PverseCityMarketplace/src/contracts/Marketplace.sol
+++ PverseCityMarketplace/src/contracts/Marketplace.sol
@@ -7,11 +7,9 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-
 import "hardhat/console.sol";
 
-contract Marketplace is ReentrancyGuard, Ownable {
+contract Marketplace is ReentrancyGuard {
 
     // Variables
     address payable public immutable feeAccount; 
